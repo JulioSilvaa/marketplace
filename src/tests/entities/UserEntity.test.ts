d7b9a5cb889e3948b02e68d112fc6fd1a0d9d3cb
Julio Silvaa <juliocesar.amancio@yahoo.com.br>
2025-12-04 22:03:24 -0300

    refactor(test): refatorado arquivo de entidade e ajustado nos testes

--- src/tests/entities/UserEntity.test.ts
+++ src/tests/entities/UserEntity.test.ts
@@ -1,17 +1,17 @@
 import { describe, expect, it } from "vitest";
-import { UserEntity } from "../core/entities/UserEntity";
-import { IUser, UserRole } from "../types/user";
+import { UserEntity } from "../../core/entities/UserEntity";
+import { IUser, UserIsActive, UserRole } from "../../types/user";
 
 describe("UserEntity", () => {
   const validUserProps: IUser = {
     id: "123e4567-e89b-12d3-a456-426614174000",
     email: "teste@example.com",
-    nome: "João Silva",
-    telefone: "999999999",
-    tipo: UserRole.cliente,
-    verificado: true,
+    name: "João Silva",
+    phone: "(15)999999999",
+    role: UserRole.CLIENTE,
+    checked: true,
     password: "securepassword123",
-    isActive: true,
+    status: UserIsActive.ATIVO,
   };
 
   describe("Criação", () => {
@@ -20,9 +20,9 @@ describe("UserEntity", () => {
       expect(user).toBeInstanceOf(UserEntity);
       expect(user.id).toBe(validUserProps.id);
       expect(user.email).toBe(validUserProps.email);
-      expect(user.name).toBe(validUserProps.nome);
-      expect(user.role).toBe(validUserProps.tipo);
-      expect(user.isActive).toBe(validUserProps.isActive);
+      expect(user.name).toBe(validUserProps.name);
+      expect(user.role).toBe(validUserProps.role);
+      expect(user.status).toBe(validUserProps.status);
     });
 
     it("deve criar uma instância de UserEntity mesmo sem ID", () => {
@@ -35,17 +35,17 @@ describe("UserEntity", () => {
 
   describe("Validação (Regras de Negócio)", () => {
     it("deve lançar um erro se o nome for vazio", () => {
-      const props = { ...validUserProps, nome: "" };
+      const props = { ...validUserProps, name: "" };
       expect(() => UserEntity.create(props)).toThrow("Nome é Obrigatorio");
     });
 
     it("deve lançar um erro se o nome for apenas espaços em branco", () => {
-      const props = { ...validUserProps, nome: "   " };
+      const props = { ...validUserProps, name: "   " };
       expect(() => UserEntity.create(props)).toThrow("Nome é Obrigatorio");
     });
 
     it("deve lançar um erro se o nome tiver menos de 3 caracteres", () => {
-      const props = { ...validUserProps, nome: "Jo" };
+      const props = { ...validUserProps, name: "Jo" };
       expect(() => UserEntity.create(props)).toThrow("Nome deve ter pelo menos 3 caracteres");
     });
 
@@ -75,7 +75,10 @@ describe("UserEntity", () => {
     });
 
     it("deve lançar um erro se o tipo (role) for inválido", () => {
-      const props = { ...validUserProps, tipo: "admin_invalido" as UserRole };
+      const props = {
+        ...validUserProps,
+        role: "admin_invalido" as unknown as UserRole,
+      };
       expect(() => UserEntity.create(props)).toThrow("Role inválida");
     });
   });
@@ -85,21 +88,21 @@ describe("UserEntity", () => {
       const user = UserEntity.create(validUserProps);
       expect(user.id).toBe(validUserProps.id);
       expect(user.email).toBe(validUserProps.email);
-      expect(user.name).toBe(validUserProps.nome);
+      expect(user.name).toBe(validUserProps.name);
       expect(user.password).toBe(validUserProps.password);
-      expect(user.role).toBe(validUserProps.tipo);
-      expect(user.isActive).toBe(validUserProps.isActive);
+      expect(user.role).toBe(validUserProps.role);
+      expect(user.status).toBe(validUserProps.status);
     });
 
     it("deve retornar true para isAdmin quando o tipo for proprietario", () => {
-      const props = { ...validUserProps, tipo: UserRole.proprietario };
+      const props = { ...validUserProps, role: UserRole.PROPRIETARIO };
       const user = UserEntity.create(props);
       expect(user.isAdmin()).toBe(true);
       expect(user.isClient()).toBe(false);
     });
 
     it("deve retornar true para isClient quando o tipo for cliente", () => {
-      const props = { ...validUserProps, tipo: UserRole.cliente };
+      const props = { ...validUserProps, tipo: UserRole.CLIENTE };
       const user = UserEntity.create(props);
       expect(user.isClient()).toBe(true);
       expect(user.isAdmin()).toBe(false);
