416e791556f33af0d49cb7ed52d73aff8480cb3e
juliosilvaa <juliocesar.amancio@yahoo.com.br>
2025-12-10 16:04:41 -0300

    feat(repositories): adiciona métodos findAll e search na interface IUserRepository

--- src/core/repositories/userRepository.ts
+++ src/core/repositories/userRepository.ts
@@ -4,6 +4,8 @@ export interface IUserRepository {
   create(data: IUser): Promise<void>;
   findByEmail(email: string): Promise<IUser | null>;
   findById(id: string): Promise<IUser | null>;
+  findAll(): Promise<IUser[]>;
+  search(filters: { name?: string; email?: string; isActive?: boolean }): Promise<IUser[]>;
   update(id: string, data: Partial<IUser>): Promise<void>;
   delete(id: string): Promise<void>;
 }
